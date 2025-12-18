a2dd9495a87faa613506b27953ca34bde43d3f49
kunal0022 <kunal.j.gajbhare@gmail.com>
2025-12-18 13:00:33 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const connectDB = require("./config/backend");
+const User =  require("./models/user");
+const app = express();
+
+app.post("/signup", async (req,res)=>{
+
+    //creating new user instance
+    const user = new User({
+        firstName: "priti",
+        lastName: "Kathar",
+        emailId: "kkathar4@gmail.com",
+        password: "kiran@123"
+    })
+    
+    try{
+        await user.save();
+        res.send("User signed up successfully");
+
+    }catch(err){
+        console.error("Error signing up user:", err);
+        res.status(400).send("Error signing up user:");
+    }
+
+})
+
+
+connectDB().then(() =>{
+    console.log("DB connected successfully");
+    app.listen(7777, () =>{console.log("Server is running on port 7777")});
+        }).catch((err) =>{
+            console.log("DB connection error", err);
+        })
+
+
---
+++ src/config/backend.js
@@ -0,0 +1,12 @@
+
+
+const mongoose = require("mongoose");
+
+//connection URL
+// const url= "mongodb+srv://namastedev:namastedev@namaste-node.e4wyns1.mongodb.net/?appName=Namaste-Node"
+const connectDB = async () =>{
+    await mongoose.connect("mongodb+srv://namastedev:namastedev@namaste-node.e4wyns1.mongodb.net/devTinder?appName=Namaste-Node");
+}
+
+module.exports = connectDB;
+
